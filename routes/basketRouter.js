ec01f912d5539c0903e1256e8c77c86edd37111c
Ivan Kazanovskiy <ivkazanovskiy@gmail.com>
2022-02-02 18:32:57 +0300

    firstSaveBasket

--- routes/basketRouter.js
+++ routes/basketRouter.js
@@ -2,7 +2,30 @@ const router = require('express').Router();
 
 router.route('/')
   .get((req, res) => {
-    res.send('BASKET router was here');
+    let cards;
+    // проверка аутентификации
+    if (true) {
+      // снимается с сессии
+      const userId = 1;
+      cards = [{
+        id: 1,
+        name: 'first Card',
+        price: '2$',
+        condition: 'very nice',
+        city: 'saint-P',
+      },
+      {
+        id: 2,
+        name: 'second Card',
+        price: '4$',
+        condition: 'bad',
+        city: 'MSK',
+      }];
+    }
+    res.render('basket', {
+      isGuest: true,
+      cards,
+    });
   });
 
 module.exports = router;
