36a00b920ccdc68d5c83e7a3ce4cea8cd7df8197
Ivan Kazanovskiy <ivkazanovskiy@gmail.com>
2022-02-03 15:51:13 +0300

    fix(hbs): fixed hbs layout and buttons

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,7 +2,32 @@ const router = require('express').Router();
 
 router.route('/')
   .get((req, res) => {
-    res.send('index router here');
+    let cards;
+    // проверка аутентификации
+    if (true) {
+      // снимается с сессии
+      const userId = 1;
+
+      // вытаскивается из базы данных
+      cards = [{
+        id: 1,
+        cardsName: 'first Card',
+        price: '2$',
+        condition: 'very nice',
+        city: 'saint-P',
+      },
+      {
+        id: 2,
+        cardsName: 'second Card',
+        price: '4$',
+        condition: 'bad',
+        city: 'MSK',
+      }];
+    }
+    res.render('index', {
+      isAutorized: true,
+      cards,
+    });
   });
 
 module.exports = router;
