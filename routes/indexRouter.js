83f2417514b9076233d043fa12b458a450682bd6
Ivan Kudryvtcev <ivan-kudryavcev-1998@mail.ru>
2022-02-03 17:20:14 +0300

    registration

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,7 +2,10 @@ const router = require('express').Router();
 
 router.route('/')
   .get((req, res) => {
-    res.send('index router here');
+    res.render('index', {
+      isAuthorized: req.session.isAuthorized,
+      name: req.session.user?.name,
+    });
   });
 
 module.exports = router;
