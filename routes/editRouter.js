f689a09abd224953facb5b61d05761a453d6b6a1
Igor Karpov <karpov.zgr@mail.ru>
2022-02-04 11:43:49 +0300

    button delete

--- routes/editRouter.js
+++ routes/editRouter.js
@@ -1,8 +1,24 @@
 const router = require('express').Router();
+const {
+  Card,
+} = require('../db/models');
 
 router.route('/')
   .get((req, res) => {
     res.render('edit');
   });
+
+router.get('/:id', async (req, res, next) => {
+  const cards = await Card.findOne({
+    where: { id: req.params.id },
+    raw: true,
+  });
+  res.redirect('edit', { cards });
+});
+
+router.delete('/delete/:id', async (req, res, next) => {
+  await Card.destroy({ where: { id: req.params.id } });
+  res.sendStatus(200);
+});
 
 module.exports = router;
