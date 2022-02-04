794b4761cbb9a96c19ea7ac249248398be838d70
Denis <chepelnikov.denis@yandex.ru>
2022-02-04 13:06:12 +0300

    add addition and edit func to cards

--- routes/editRouter.js
+++ routes/editRouter.js
@@ -1,17 +1,39 @@
 const router = require('express').Router();
+
 const { Card } = require('../db/models');
+
+router.get('/', (req, res) => {
+  res.render('addcard');
+});
+
+router.post('/', async (req, res) => {
+  const {
+    cardsName, img, condition, price,
+  } = req.body;
+  try {
+    await Card.create({
+      cardsName,
+      img,
+      condition,
+      price,
+      userId: req.session.user.id,
+    });
+    res.json({ status: 'Ok' });
+  } catch (error) {
+    res.json({ status: error.message });
+  }
+});
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
   const oneCard = await Card.findOne({
     where: { id },
+    raw: true,
   });
   res.render('edit', { oneCard });
 });
 
 router.put('/:id', async (req, res) => {
-  const { id } = req.params;
-  console.log(id, '<-------');
   const {
     cardsName, price, img, condition,
   } = req.body;
@@ -22,7 +44,7 @@ router.put('/:id', async (req, res) => {
       img,
       condition,
     }, {
-      where: { id },
+      where: { id: req.params.id },
     });
     res.json({ status: 'Ok' });
   } catch (error) {
